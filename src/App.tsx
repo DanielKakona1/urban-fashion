598ba87727f51a695ae19b7f2a37ce2b37c98e65
Danielkakona <Daniel@bridgelabs.design>
2020-09-20 23:23:16 +0200

    navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="container">
+      <div className="navbar">
+        <img src={require('./assets/logo.png')} alt="" className="logo" />
+        <div className="navLink">
+          <p>Home</p>
+          <p>About</p>
+          <p>Collections</p>
+          <p>Category</p>
+        </div>
+        <img src={require('./assets/cart.png')} alt="" className="cart" />
+      </div>
+    </div>
+  )
 }
 
 export default App
