50c56974a38a2943b0c373107fc40b8cbedac0b3
Danielkakona <Daniel@bridgelabs.design>
2020-09-21 00:03:19 +0200

    content

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,22 @@ function App() {
         </div>
         <img src={require('./assets/cart.png')} alt="" className="cart" />
       </div>
+      <div className="content">
+        <h5 className='btn'>2020 Collections</h5>
+        <h1>You should always <br></br>feel pretty</h1>
+        <p>Clothes that are sure heat up your winter</p>
+        <div className="arrow-icons">
+          <img src={require('./assets/back-arrow.png')} alt=""/>
+          <img src={require('./assets/next-arrow.png')} alt=""/>
+        </div>
+      </div>
+      <img src={require('./assets/img.png')} alt="" className='feature-img'/>
+
+      <div className="social-links">
+        <p>FACEBOOK</p>
+        <p>INSTAGRAM</p>
+        <p>TWITTER</p>
+      </div>
     </div>
   )
 }
